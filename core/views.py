61f5cb82f5aa6d3a9fd3ba7a00ff074ca9a5265e
Julio Paiva <juliopaiva.ti@hotmail.com>
2026-03-15 16:31:00 -0300

    feat: add initial views, routes, and templates

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    context = {
+        "key": "Django"
+    }
+    return render(request, 'index.html', context)
