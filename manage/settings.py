ecebdbbdcf2d19c0a295997565a5c7d4b89db1ee
Julio Paiva <juliopaiva.ti@hotmail.com>
2026-03-17 18:33:58 -0300

    feat: add bootstrap dependency

--- manage/settings.py
+++ manage/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "django_bootstrap5",
+
     "core",
+
 ]
 
 MIDDLEWARE = [
