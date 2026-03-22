7c630a00484ae0919c1a5ac4d680a0da35d72cf3
Julio Paiva <juliopaiva.ti@hotmail.com>
2026-03-22 15:48:36 -0300

    fix(email): rm send email with smtp

--- manage/settings.py
+++ manage/settings.py
@@ -129,12 +129,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # production
 LOGOUT_REDIRECT_URL = "/"
 
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "juliopaiva.ti@gmail.com")
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", DEFAULT_FROM_EMAIL)
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# EMAIL_HOST = "smtp.gmail.com"
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", DEFAULT_FROM_EMAIL)
+# EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 
 
 LOGGING = {
